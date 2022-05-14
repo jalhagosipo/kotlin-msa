2f79dc5e9f4c9b3ce95dfdbcbe892727de41c3c0
jalhagosipo <gpfla757@naver.com>
2022-05-14 17:28:23 +0900

    feat: 명시적으로 빈 만들기

--- src/main/kotlin/com/microservices/chapter2/Chapter2Application.kt
+++ src/main/kotlin/com/microservices/chapter2/Chapter2Application.kt
@@ -3,6 +3,7 @@ package com.microservices.chapter2
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.context.annotation.Bean
 import org.springframework.stereotype.Controller
 import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.RequestMapping
@@ -10,7 +11,11 @@ import org.springframework.web.bind.annotation.RequestMethod
 import org.springframework.web.bind.annotation.ResponseBody
 
 @SpringBootApplication
-class Chapter2Application
+class Chapter2Application {
+	// 빈으로 노출되지 않도록 concrete class가 아닌 인터페이스를 반환하는 함수를 정의
+	@Bean
+	fun exampleService(): ServiceInterface = ExampleService()
+}
 
 
 @Controller
