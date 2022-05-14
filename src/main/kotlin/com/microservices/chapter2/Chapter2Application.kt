c6aa83db476442236e3a1f4a67c312209c968904
jalhagosipo <gpfla757@naver.com>
2022-05-14 17:26:30 +0900

    feat: profile, simple service 추가

--- src/main/kotlin/com/microservices/chapter2/Chapter2Application.kt
+++ src/main/kotlin/com/microservices/chapter2/Chapter2Application.kt
@@ -1,10 +1,29 @@
 package com.microservices.chapter2
 
+import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.stereotype.Controller
+import org.springframework.web.bind.annotation.PathVariable
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RequestMethod
+import org.springframework.web.bind.annotation.ResponseBody
 
 @SpringBootApplication
 class Chapter2Application
+
+
+@Controller
+//class FirstController(val exampleService: ExampleService) {
+class FirstController() {
+
+	@Autowired
+	lateinit var service: ServiceInterface
+
+	@RequestMapping(value = ["/user/{name}"], method = [RequestMethod.GET])
+	@ResponseBody
+	fun hello(@PathVariable name: String) = service.getHello(name)
+}
 
 fun main(args: Array<String>) {
 	runApplication<Chapter2Application>(*args)
