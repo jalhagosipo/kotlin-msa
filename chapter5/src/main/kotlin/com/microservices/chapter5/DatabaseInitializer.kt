fa1d21db5bdc02be69c1517d35f31a100c57ce46
jalhagosipo <gpfla757@naver.com>
2022-05-21 17:11:27 +0900

    feat: use initialCustomers for save

--- chapter5/src/main/kotlin/com/microservices/chapter5/DatabaseInitializer.kt
+++ chapter5/src/main/kotlin/com/microservices/chapter5/DatabaseInitializer.kt
@@ -1,5 +1,6 @@
 package com.microservices.chapter5
 
+import com.microservices.chapter5.Customer.Telephone
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.data.mongodb.core.ReactiveMongoOperations
 import org.springframework.stereotype.Component
@@ -13,6 +14,14 @@ class DatabaseInitializer {
     @Autowired
     lateinit var mongoOperations: ReactiveMongoOperations
 
+    companion object {
+        val initialCustomers = listOf(
+            Customer(1, "Kotlin"),
+            Customer(2, "Spring"),
+            Customer(3, "Microservice", Telephone("+44", "7123456789"))
+        )
+    }
+
     @PostConstruct
     fun initData() {
         mongoOperations.collectionExists("Customers").subscribe {
@@ -22,7 +31,7 @@ class DatabaseInitializer {
                 }
             } else {
                 println("Customers collections already exist")
-                customerRepository.save(Customer(1, "spring"))
+                customerRepository.saveAll(initialCustomers)
                     .subscribe {
                         println("Default customers created")
                     }
