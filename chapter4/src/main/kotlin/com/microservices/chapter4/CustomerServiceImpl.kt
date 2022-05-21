9ba1c8c56ff006f859ae446d94dd2617aef7224c
jalhagosipo <gpfla757@naver.com>
2022-05-21 13:44:33 +0900

    feat: 리액티브하게 객체 수신

--- chapter4/src/main/kotlin/com/microservices/chapter4/CustomerServiceImpl.kt
+++ chapter4/src/main/kotlin/com/microservices/chapter4/CustomerServiceImpl.kt
@@ -2,6 +2,7 @@ package com.microservices.chapter4
 
 import com.microservices.chapter4.Customer.Telephone
 import org.springframework.stereotype.Component
+import reactor.core.publisher.Mono
 import reactor.kotlin.core.publisher.toFlux
 import reactor.kotlin.core.publisher.toMono
 import java.util.concurrent.ConcurrentHashMap
@@ -27,4 +28,12 @@ class CustomerServiceImpl : CustomerService {
                 it.value.name.contains(nameFilter, true)
             }.map{ it.value }.toFlux()
 
+    override fun createCustomer(customerMono: Mono<Customer>): Mono<*> {
+        return customerMono.map {
+            customers[it.id] = it
+            // 이렇게 그냥 두면 빈객체{}를 리턴. 수신된 모노엣 매퍼(mapper)를 사용해 변환하기 때문. 추가 객체를 생성하지 않았으므로 빈 객체가 반환됨
+            // it // 일반 post 한것과 같은 동일한 객체 리턴
+            // Mono.empty<Any>() // 명시적 빈객체
+        }
+    }
 }
