87edc927174fe3b46a183899ad80e415de6d70a5
jalhagosipo <gpfla757@naver.com>
2022-05-21 17:43:44 +0900

    feat: get handler 추가

---
+++ chapter5/src/main/kotlin/com/microservices/chapter5/CustomerRouter.kt
@@ -0,0 +1,17 @@
+package com.microservices.chapter5
+
+import org.springframework.context.annotation.Bean
+import org.springframework.stereotype.Component
+import org.springframework.web.reactive.function.server.router
+
+@Component
+class CustomerRouter(
+    private val customerHanlder: CustomerHandler
+) {
+    @Bean
+    fun customerRoutes() = router {
+        "/customer".nest {
+            GET("/{id}", customerHanlder::get)
+        }
+    }
+}
