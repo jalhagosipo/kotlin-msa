79890761f84b878e7692909aa31a277d541fcde7
jalhagosipo <gpfla757@naver.com>
2022-05-31 22:35:12 +0900

    feat: 컨피그 서버 만들기

--- chapter6-configserver/src/main/kotlin/com/microservices/chapter6/Chapter6Application.kt
+++ chapter6-configserver/src/main/kotlin/com/microservices/chapter6/Chapter6Application.kt
@@ -2,8 +2,10 @@ package com.microservices.chapter6
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.cloud.config.server.EnableConfigServer
 
 @SpringBootApplication
+@EnableConfigServer
 class Chapter6Application
 
 fun main(args: Array<String>) {
