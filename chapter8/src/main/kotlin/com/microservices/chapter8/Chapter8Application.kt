fd31913bd0780a5bed368847f25834dd0c0b65a1
jalhagosipo <gpfla757@naver.com>
2022-06-06 19:58:52 +0900

    feat: 컨트롤러 추가 및 도커파일 추가

--- chapter8/src/main/kotlin/com/microservices/chapter8/Chapter8Application.kt
+++ chapter8/src/main/kotlin/com/microservices/chapter8/Chapter8Application.kt
@@ -2,8 +2,6 @@ package com.microservices.chapter8
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
-import org.springframework.web.bind.annotation.GetMapping
-import org.springframework.web.bind.annotation.RestController
 
 @SpringBootApplication
 class Chapter8Application
