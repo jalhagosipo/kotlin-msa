000b774938b169ff9ee85e1fc3c7196eb33d365d
jalhagosipo <gpfla757@naver.com>
2022-06-17 14:27:55 +0900

    feat: use MockBean

--- chapter9/src/test/kotlin/com/microservices/chapter9/CustomerControllerTest.kt
+++ chapter9/src/test/kotlin/com/microservices/chapter9/CustomerControllerTest.kt
@@ -4,6 +4,7 @@ import org.junit.jupiter.api.Test
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc
 import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.boot.test.mock.mockito.MockBean
 import org.springframework.test.web.servlet.MockMvc
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers.status
@@ -15,6 +16,9 @@ import org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPat
 class CustomerControllerTest {
     @Autowired
     lateinit var mockMvc: MockMvc
+
+    @MockBean
+    lateinit var customerService: CustomerService
 
     @Test
     fun `mock mvc should be configured`() {
